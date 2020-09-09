a1bad10f04884809c1edbde64c812ebf526afa0d
Alon Weissfeld <alonweissfeld@Alons-MacBook-Pro.local>
2020-09-09 12:48:11 +0300

    Added basic html/css for the user header. In addition, added logic to display a red circle indicating that this user is live now and show an appropriate link.

--- sellotape/sellotape_dj/main_app/views.py
+++ sellotape/sellotape_dj/main_app/views.py
@@ -8,8 +8,15 @@ def index(request):
 def user(request, username):
     # Get the profile for the passed username or raise 404 if not found.
     profile = get_object_or_404(User, username=username)
+
+    # TODO: query the user streams once the strams table is merged.
+    streams = []
+    is_live = len(streams) and streams[0]['ended_on'] == None
+    live_link = is_live and streams[0]['link']
     
     context = {
         'profile': profile,
+        'streams': streams,
+        'live_link': live_link,
     }
     return render(request, 'sellotape/user.html', context)
