a96680ede9c9c68a48c64589c971b4aaa5a39680
Alon Weissfeld <alonweissfeld@Alons-MacBook-Pro.local>
2020-09-09 11:28:45 +0300

    Added basic username routing. Given an exisitng username endpoint (/username/) we query that user and pass on the relevant params to the template.

--- sellotape/sellotape_dj/main_app/views.py
+++ sellotape/sellotape_dj/main_app/views.py
@@ -1,4 +1,21 @@
+from django.http import Http404
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 def index(request):
     return render(request, 'sellotape/index.html')
+
+def user(request, username):
+    # Get the query set profile for the passed username
+    qs = User.objects.filter(username=username)
+    if not qs.exists():
+        raise Http404
+
+    profile = qs.first()
+    context = {
+        'username': username,
+        'first_name': profile.first_name,
+        'last_name': profile.last_name,
+        'email': profile.email
+    }
+    return render(request, 'sellotape/user.html', context)
