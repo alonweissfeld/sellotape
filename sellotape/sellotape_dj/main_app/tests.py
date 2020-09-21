d8d5d2d857486aa1a2b926ea3d28a8f2f9ec0d01
Alon Weissfeld <alonweissfeld@Alons-MacBook-Pro.local>
2020-09-21 13:18:09 +0300

    Added two tests for the user view:
    - Return 404 if the user does not exist.
    - Display an appropriate message when a user exist but has no streams.
    
    In addition, added the relevant html and tempalte conditions to show 'no streams are available'.
    Note that the main_app namespace was added to the main urls.py file of the project.

--- sellotape/sellotape_dj/main_app/tests.py
+++ sellotape/sellotape_dj/main_app/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from django.contrib.auth.models import User
+
+def create_user_with_streams(username, streams):
+    """
+    Create a user with the given streams.
+    """
+    return User.objects.create(username=username)
+
+class UserViewTests(TestCase):
+    def test_user_does_not_exist(self):
+        """
+        If user does not exist, it returns a 404.
+        """
+        url = reverse('main_app:user', args=('non-exist',))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+
+    def test_no_streams(self):
+        """
+        If no streams are available, an appropriate message is displayed.
+        """
+        username = 'darth-vader'
+        create_user_with_streams(username, [])
+
+        url = reverse('main_app:user', args=(username,))
+        response = self.client.get(url)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "No streams are available.")
+        self.assertQuerysetEqual(response.context['future_streams'], [])
+        self.assertQuerysetEqual(response.context['previous_streams'], [])
